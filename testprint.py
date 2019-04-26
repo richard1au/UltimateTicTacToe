moved heuristic function to board, fixed calculation bug

testprint.py
@@ -1,6 +1,10 @@
 from board import Board
 import numpy as np
 
+#Make test board, print state, then place, print state then commence testing
 testBoard = Board(np.zeros((10,10), dtype = "int8"))
-testBoard.place(2,2,1)
 testBoard.print_board()
+testBoard.place(1,2,2)
+children = testBoard.generate_children()
+for child in children:
+    child.print_board()