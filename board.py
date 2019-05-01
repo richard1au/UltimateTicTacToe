5bd6654dacf81bfffa8b633602adfe0cc7fd4b9d
Richard Lau <z5163179@student.unsw.edu.au>
2019-05-01 22:31:10 +1000

    Cleaned up board file, commented wher it is no obvious

--- board.py
+++ board.py
@@ -1,10 +1,9 @@
 import numpy as np
 from copy import copy
 
-#Do note that this board class functions work heavily under the 
-#assumption that a randomly made first move has been done in order to generate children!
+#Board Class is a representation of a 9x9 tic tac toe board
 class Board:
-
+    #Contains storage of (9x9) numpy array along with various useful attributes of the current state of game
     def __init__(self, state, heurCurr = 0):
         self._state = state
         self._boardplayedin = 0
@@ -15,54 +14,29 @@ class Board:
     @property
     def state(self):
         return self._state
-
-    #remember for agent.py heuristic is computed from player 1 point of view
-    #called player 1= 1, player2 = 2
-    #compute heuristic can now be changed to just return self._heurCurr
+     
     def compute_heuristic(self, player1, player2):
-        #return heuristicBoard(self, player1, player2)
         return self._heurCurr
     
+    #Retrives either 0,1,2 at the specified board and position (num)
     def get_tile(self, board, num):
         return self._state[board][num]
 
+    #Places player indiciator at specified board and position(num), updates attributes of board state
     def place(self, board, num, player):
-        #Also need to update heur by only checking one board heur before and heur now (hashing will speed this up significantly)
-        #Could also split up heuristic modification and board placement into two functions? Not necessary
         prevHeur = heuristicSmall(self, board, 1, 2)
         self._state[board][num] = player
         self._boardplayedin = board
         self._boardtoplayin = num
         self._player = player
-        #ie. If prevheur was 5 in the same board, now its 10, then update by +5!
         newHeur = heuristicSmall(self, board, 1, 2)
         self._heurCurr += newHeur - prevHeur
         
-
-    def print_board_row(self, a, b, c, i, j, k):
-        # The marking script doesn't seem to like this either, so just take it out to submit
-        print("", self._state[a][i], self._state[a][j], self._state[a][k], end = " | ")
-        print(self._state[b][i], self._state[b][j], self._state[b][k], end = " | ")
-        print(self._state[c][i], self._state[c][j], self._state[c][k])
-
-    def print_board(self):
-        self.print_board_row(1,2,3,1,2,3)
-        self.print_board_row(1,2,3,4,5,6)
-        self.print_board_row(1,2,3,7,8,9)
-        print(" ------+-------+------")
-        self.print_board_row(4,5,6,1,2,3)
-        self.print_board_row(4,5,6,4,5,6)
-        self.print_board_row(4,5,6,7,8,9)
-        print(" ------+-------+------")
-        self.print_board_row(7,8,9,1,2,3)
-        self.print_board_row(7,8,9,4,5,6)
-        self.print_board_row(7,8,9,7,8,9)
-        print()
-        #print("heuristic: ", self.compute_heuristic(), "\n")
-
+    #Determines if the board is in a finishing state (either full or won)
     def is_terminal(self):
         return self.boardfull() or self.won()
 
+    #Board is full when there are no empty positions (0)
     def boardfull(self):
         for i in range(1, 9):
             for j in range(1, 9):
@@ -71,6 +45,7 @@ class Board:
 
         return True
 
+    #Board is won where there is three in a row
     def won(self):
         b = self._state[self._boardplayedin]
         p = self._player
@@ -83,13 +58,14 @@ class Board:
                 b[1]==b[5]==b[9]==p or
                 b[3]==b[5]==b[7]==p)
 
+    #Generates next possible game states to one depth in a list
     def generate_children(self):
         children = []
-        b = self._state[self._boardtoplayin]
-        for i in range(1,10):
-            if b[i] == 0:
-                newBoard = Board(copy(self._state), self._heurCurr)
-                newBoard.place(self._boardtoplayin, i, self.next_player())
+        b = self._state[self._boardtoplayin]  #Gets the board to be played in
+        for i in range(1,10): 
+            if b[i] == 0: #Checks all positions on board, if empty there is a valid move
+                newBoard = Board(copy(self._state), self._heurCurr) #Create new Board object with copied state and current heuristic
+                newBoard.place(self._boardtoplayin, i, self.next_player()) #Place new valid move
                 children.append(newBoard)
 
         return children
@@ -97,6 +73,7 @@ class Board:
     def get_player(self):
         return self._player
 
+    #Tells us which player is to play after this board state
     def next_player(self):
         if self._player == 2:
             return 1
@@ -105,18 +82,6 @@ class Board:
     def get_boardtoplayin(self):
         return self._boardtoplayin
         
-
-
-# calculates the heuristic of the entire board
-# where player1 is the player who called the alpha beta search
-#player1 = 1, player2 = 2
-def heuristicBoard(self, player1, player2):
-    total = 0
-    for i in range(1,10):
-        total += heuristicSmall(self, i, player1, player2)
-    return total
-
-
 # evaluates the heuristic for a 3x3 board
 def heuristicSmall(self, i, player1, player2):
     total1 = 0
@@ -130,7 +95,7 @@ def heuristicSmall(self, i, player1, player2):
             curr1 += 1
         if self.get_tile(i,k) == player2:
             curr2 += 1
-        if k % 3 == 0:
+        if k % 3 == 0: #Partitions totals for [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
             total1 += heuristicAddX(curr1, curr2)
             total2 += heuristicAddO(curr1, curr2)
             curr1 = 0
@@ -139,7 +104,7 @@ def heuristicSmall(self, i, player1, player2):
     #calculates for each column
     for k in range(0,3):
         for l in range(1,8,3):
-            if self.get_tile(i,l+k) == player1:
+            if self.get_tile(i,l+k) == player1: #l+k makes sure we are in the correct column. k iterating which column we look at
                 curr1 += 1
             if self.get_tile(i,l+k) == player2: 
                 curr2 += 1      
@@ -149,7 +114,7 @@ def heuristicSmall(self, i, player1, player2):
         curr2 = 0
                 
     #calculates for each diagonal 
-    for k in range(1,10,4):
+    for k in range(1,10,4): #Positions (1, 5, 9)
         if self.get_tile(i,k) == player1:
             curr1 += 1
         if self.get_tile(i,k) == player2:
@@ -159,7 +124,7 @@ def heuristicSmall(self, i, player1, player2):
     curr1 = 0
     curr2 = 0
         
-    for k in range(3,9,2):
+    for k in range(3,8,2): #Positions (3, 5, 7)
         if self.get_tile(i,k) == player1:
             curr1 += 1
         if self.get_tile(i,k) == player2:
@@ -169,31 +134,31 @@ def heuristicSmall(self, i, player1, player2):
     curr1 = 0
     curr2 = 0
     
-    return total1 - total2
+    return total1 - total2 #total player 1 - total player 2 gives us a zero sum game.
 
 
-#helper function to find 1 score for a row/col/diagonal
+#helper function to find player 1 (us) score for a row/col/diagonal
 def heuristicAddX(curr1, curr2):
     if curr2 == 0:
-        if curr1 == 3:
+        if curr1 == 3: #10,000 points for three in a row
             return 10000
-        if curr1 == 2:
+        if curr1 == 2: #20 points for two in a row
             return 20
-        if curr1 == 1:
+        if curr1 == 1: #1 point for just one piece
             return 1
         else:
             return 0
     else:
         return 0
         
-#helper function to find 2 score for a row/col/diagonal
+#helper function to find player 2 (enemy) score for a row/col/diagonal
 def heuristicAddO(curr1, curr2):
     if curr1 == 0:
-        if curr2 == 3:
-            return 10000
-        if curr2 == 2:
+        if curr2 == 3: #10,000 points for three in a row
+            return 10000 
+        if curr2 == 2: #20 points for two in a row
             return 20
-        if curr2 == 1:
+        if curr2 == 1: #1 point for just one piece
             return 1
         else:
             return 0 
